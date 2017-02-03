b3e4b9fc1d4f9790f993fa412cf1edaee7bbc74d
nbogis <bent_al7ejaz90@hotmail.com>
2017-02-03 11:21:33 -0600

    user can unlike post or comment. unlike icon only shows when user already liked. like icon only shows when user hasn't liked

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  private
   def require_login
     unless user_signed_in?
       flash[:error] = "You need to sign in before accessing WajhBook"
       redirect_to sign_in_path
     end
   end
+
+  def user_liked_model(user, liked_id, liked_type)
+    Like.where(:user_id => user.id,
+              :likeable_type => liked_type,
+              :likeable_id => liked_id)
+  end
+  helper_method :user_liked_model
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -16,4 +24,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :email, :password, :profile_pic, :cover_pic])
   end
+
 end
