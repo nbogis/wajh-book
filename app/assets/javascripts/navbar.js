13b9ed61db0de451b808364ef618eda6e400b25d
nbogis <bent_al7ejaz90@hotmail.com>
2017-02-19 18:25:36 -0600

    attempt adding search bar. results are not viewed yet

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -5,28 +5,51 @@ jQuery(function() {
     $(this).addClass('active');
   });
 
-  $('.ui.search')
-  .search({
+  $('.ui.search').search({
     apiSettings: {
-    action: 'search',
-    url: '/users/search_users.json?query={query}',
-    debug: 1,
-    onResponse (response) {
-      return {
-        results: response.myresults
+      url: '/users/search.json?query={query}',
+      // onResponse (response) {
+      //   return {
+      //     results: response.users
+      //   };
+      //   response.users.forEach((item) => {
+      //     item.title = item.username;
+      //   });
+      // },
+
+      onResponse : function(theresponse) {
+        console.log(theresponse);
+        var response = {
+            results : {}
+          };
+
+        $.each(theresponse.users, function(index, user) {
+          console.log(user);
+          var username = user.username || 'Unknown',  maxResults = 8;
+
+          if(response.results[username] === undefined) {
+            response.results[username] = {
+                name: username,
+                results: []
+            };
+          }
+          // add result to category
+          response.results[username].results.push({
+            title       : user.username,
+            description : user.first_name,
+            url         : user.last_name
+          });
+        return response;
+        });
       }
-      response.myresults.forEach((item) => {
-        item.title = item.name;
-      });
-    }
-  },
-  fields: {
-      results: 'items',
-      title: 'name',
-      url: 'html_url',
-      description: 'description'
-  },
-    searchFullText: false
-  })
-;
+    },
+    type: 'standard',
+    source : users,
+    searchFields   : [
+        'username',
+        'first_name',
+        'last_name'
+    ],
+    searchFullText: false,
+  });
 });
